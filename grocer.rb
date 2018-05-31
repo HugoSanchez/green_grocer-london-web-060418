c8f59dcbedb03a9311ee8b94dcb285bfe55b7394
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-31 12:53:19 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,18 @@
+require "pry"
+
 def consolidate_cart(cart)
   consolidated_cart = Hash.new
-  cart.each do |item|
+  cart.each do |item, values|
     if consolidated_cart[item]
+      consolidated_cart << item
+    else consolidated_cart[item] = {}
+      consolidated_cart << values
+      binding.pry
+    end
+  end
+end
+      
+      
 end
 
 def apply_coupons(cart, coupons)
