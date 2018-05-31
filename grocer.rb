068080573d0bf78772b46922d157a400fffc8d7a
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-31 13:12:15 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -5,9 +5,7 @@ def consolidate_cart(cart)
   consolidated_cart = Hash.new
   
   cart.uniq.each do |item|
-    if consolidated_cart[item]
-      consolidated_cart[x][:count] = cart.each {|name| counts[name] += 1
-    else consolidated_cart[x]
+    puts item
     end
   end
   binding.pry
